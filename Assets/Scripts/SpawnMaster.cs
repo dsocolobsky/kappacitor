9e16927b97f53dacf14b0a8b448fd5e507cbe7d8
Dylan Socolobsky <dsocolobsky@gmail.com>
2016-06-09 02:28:14 -0300

    Spawners y musica y paredes

---
+++ Assets/Scripts/SpawnMaster.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnMaster : MonoBehaviour {
+
+    public int enemies;
+    int numberEnemies;
+    GameObject[] spawners;
+
+    // Use this for initialization
+    void Start () {
+        spawners = GameObject.FindGameObjectsWithTag("spawner");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject[] enemyArray;
+        enemyArray = GameObject.FindGameObjectsWithTag("enemy");
+        numberEnemies = enemyArray.Length;
+
+        if (numberEnemies < enemies)
+        {
+            GameObject s;
+            do
+            {
+                s = spawners[Mathf.FloorToInt(Random.Range(0, spawners.Length))];
+                s.GetComponent<Spawner>().Spawn();
+            } while (insideCamera(s.transform.position));
+        }
+    }
+
+    bool insideCamera(Vector3 transform)
+    {
+        return (Camera.main.WorldToViewportPoint(transform).x >= 0 &&
+            Camera.main.WorldToViewportPoint(transform).x <= 1) &&
+            (Camera.main.WorldToViewportPoint(transform).y >= 0 &&
+            Camera.main.WorldToViewportPoint(transform).y <= 1);
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject obj;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Spawn()
+    {
+        Instantiate(obj, transform.position, Quaternion.identity);
+    }
+}
