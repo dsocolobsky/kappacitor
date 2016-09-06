fcac39576c7da7bc7d88fb260a4ce0888846f4d8
Dylan Socolobsky <dsocolobsky@gmail.com>
2016-09-05 22:06:06 -0300

    Añadir funcionalidad basica del Led

---
+++ Assets/Scripts/ChangeLedAnimation.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeLedAnimation : ChangeAnimation
+{
+
+    Led.State state = Led.State.MOVING;
+
+    // Use this for initialization
+    void Start()
+    {
+        prefix = "led_";
+        previousAnimation = "down";
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Change(Led.State state, float horizontal, float vertical)
+    {
+        this.state = state;
+
+        if (vertical == 0 && horizontal == 0)
+        {
+            animator.Play(previousAnimation, 0, 0);
+        }
+
+        if (vertical == -1)
+        {
+            if (horizontal == -1)
+            {
+                play("left");
+            }
+            else if (horizontal == 1)
+            {
+                play("right");
+            }
+            else {
+                play("down");
+            }
+        }
+        else if (vertical == 1)
+        {
+            if (horizontal == -1)
+            {
+                play("left");
+            }
+            else if (horizontal == 1)
+            {
+                play("right");
+            }
+            else {
+                play("up");
+            }
+        }
+        else
+        {
+            if (horizontal == -1)
+            {
+                play("left");
+            }
+            else if (horizontal == 1)
+            {
+                play("right");
+            }
+        }
+    }
+
+    protected new void play(string anim)
+    {
+        if (state == Led.State.MOVING)
+            animator.Play("led_" + anim);
+        else if (state == Led.State.ATTACKING)
+            animator.Play("led_attacking");
+        else if (state == Led.State.EXPLODING)
+            animator.Play("led_exploding");
+
+        previousAnimation = anim;
+    }
+}
---
+++ Assets/Scripts/Led.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Led : MonoBehaviour
+{
+    public enum State
+    {
+        MOVING,
+        ATTACKING,
+        EXPLODING
+    }
+
+    public State state = State.MOVING;
+
+    GameObject player;
+    bool detectedPlayer;
+
+    public float speed;
+
+    ChangeLedAnimation changeAnimation;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        detectedPlayer = false;
+        changeAnimation = GetComponent<ChangeLedAnimation>();
+    }
+
+    void FixedUpdate()
+    {
+        if (!player) return;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, player.transform.position - transform.position);
+        detectedPlayer = hit.collider.tag == "Player";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (state != State.MOVING)
+        {
+            this.speed = 0.0f;
+        }
+
+        if (detectedPlayer)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        }
+
+        // TODO: Explotar
+        float distance = Vector3.Distance(player.transform.position, transform.position);
+        if (distance < 0.2f && state == State.MOVING)
+        {
+            state = State.ATTACKING;
+        }
+
+        Vector3 direction = getDirection();
+        changeAnimation.Change(state, direction.x, direction.y);
+    }
+
+    public Vector3 getDirection()
+    {
+        Vector3 retdir = new Vector3();
+        Vector3 direction = player.transform.position - transform.position;
+
+        if (direction.x > -0.75f && direction.x < 0.75f)
+        {
+            retdir.x = 0;
+        }
+        else if (direction.x < -0.75f)
+        {
+            retdir.x = -1;
+        }
+        else {
+            retdir.x = 1;
+        }
+
+        if (direction.y < 0)
+        {
+            retdir.y = -1;
+        }
+        else if (direction.y > 0)
+        {
+            retdir.y = 1;
+        }
+
+        return retdir;
+    }
+
+    public void ChangeState(State state)
+    {
+        this.state = state;
+    }
+
+    public void Destroy()
+    {
+        Destroy(this.gameObject);
+    }
+}
